71ae8aa9e7a3de2647c666145b10f3dfb504d695
Tim Andrews <timpandrews@yahoo.com>
2023-02-01 16:55:59 -0500

    Fixed issue with migrations

--- apps/garage/models.py
+++ apps/garage/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 class Doc(models.Model):
-    doc_id = models.Field(primary_key = True)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length = 200)
     text = models.TextField()
+    char = models.CharField(max_length = 200)
 
     def __str__(self):
         return self.title
