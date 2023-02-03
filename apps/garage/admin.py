0cc09e13014af01b228a1c59f28c1d946fec2149
Tim Andrews <timpandrews@yahoo.com>
2023-02-03 14:02:30 -0500

    rename title field to data_type

--- apps/garage/admin.py
+++ apps/garage/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Doc
 
 class DocAdmin(admin.ModelAdmin):
-    fields = ("title", "user_id", "data")
-    list_display = ("id", "title", "user_id", "data")
-    list_display_links = ("id", "title")
+    fields = ("user_id", "data_type", "data")
+    list_display = ("id", "user_id", "data_type", "data")
+    list_display_links = ("id", "user_id", "data_type")
 
 admin.site.register(Doc, DocAdmin)
--- apps/garage/models.py
+++ apps/garage/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Doc(models.Model):
-    title = models.CharField(max_length = 200)
     user_id = models.IntegerField(default=1)
+    data_type = models.CharField(max_length = 200)
     data = models.JSONField()
 
 
     def __str__(self):
-        return self.title
+        return self.data_type
