e78c98bc074b253df1503c149fd8fe054af3dd4c
Tim Andrews <timpandrews@yahoo.com>
2023-02-03 10:25:29 -0500

    Changed field to JSONField

--- apps/garage/admin.py
+++ apps/garage/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Doc
 
-admin.site.register(Doc)
+class DocAdmin(admin.ModelAdmin):
+    fields = ("title", "data")
+    list_display = ("id", "title", "data")
+    list_display_links = ("id", "title")
+
+admin.site.register(Doc, DocAdmin)
--- apps/garage/models.py
+++ apps/garage/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Doc(models.Model):
     title = models.CharField(max_length = 200)
-    text = models.TextField()
-    char = models.CharField(max_length = 200)
+    data = models.JSONField()
+
 
     def __str__(self):
         return self.title
