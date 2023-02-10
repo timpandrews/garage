cdd64a9237b7a51c39fbee024c17de7ae83c7427
Tim Andrews <timpandrews@yahoo.com>
2023-02-10 10:31:38 -0500

    remove unused view & dashboard

--- apps/garage/views.py
+++ apps/garage/views.py
@@ -4,24 +4,9 @@ from django.shortcuts import render
 from .models import Doc
 
 
-def home(response):
-    return render(response, "garage/home.html", {})
-    if response.method == "POST":
-        form = AddNewRide(response.POST)
-        if form.is_valid():
-            print(form.cleaned_data)
-            title = "ride"
-            user_id = 1  # placeholder for user table
-            data = form.cleaned_data
-            data["duration"] = data["duration"].total_seconds()
-            ride = Doc(data_type="ride", user_id=user_id, data=data)
-            ride.save()
+def landing(response):
+    return render(response, "garage/landing.html", {})
 
-            return HttpResponseRedirect("/")
 
-    else:
-        form = AddNewRide()
-        context = {
-            "form": form,
-        }
-        return render(response, "garage/add_new.html", context)
+def dashboard(response):
+    return render(response, "garage/dashboard.html", {})
