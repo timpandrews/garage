838ded0551292e4ad61d84ce8f6bfbe9134256b4
Tim Andrews <timpandrews@yahoo.com>
2023-02-11 16:14:43 -0500

    Fix (Temp?) for #11

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+
+@login_required
 def dashboard(response):
     return render(response, "dashboard/dashboard.html", {})
