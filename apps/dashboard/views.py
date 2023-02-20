1431589b6301a4f1c2a94be501c6676ebc666829
Tim Andrews <timpandrews@yahoo.com>
2023-02-20 13:49:59 -0500

    Month by Month comparison chart

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,5 +1,8 @@
 import datetime
+from datetime import timedelta, timezone
 
+import pytz
+from dateutil import rrule
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Sum
@@ -42,7 +45,53 @@ def dashboard(request):
 
 
 class db_month(LoginRequiredMixin, TemplateView):
+    colors = ["#827BC5","#7D8B96","#15807C","#332F32","#C8C1BB","#AB4738","#5B5963"]
     template_name = "dashboard/month.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        earliest_ride = Doc.objects.filter(user=self.request.user, active=True).earliest()
+        start_date = earliest_ride.data_date
+        first_date = start_date.replace(day=1)
+        current_date = datetime.datetime.now(timezone.utc)
+
+        month_year = []
+        milage = []
+        bgColor = []
+
+        # get 1st color for columns
+        i = 1
+        color = get_color(self.colors, i)
+
+        for dt in rrule.rrule(rrule.MONTHLY, dtstart=first_date, until=current_date):
+            year = str(dt.strftime('%Y'))
+            month = str(dt.strftime('%m'))
+            month_year_str = str(dt.strftime('%b %Y'))
+            month_year.append(month_year_str)
+
+            # get new color for columns for each new year
+            if month == "01":
+                i += 1
+                color = get_color(self.colors, i)
+            bgColor.append(color)
+
+            # get monthly milage
+            miles_this_year = 0
+            rides = Doc.objects.filter(
+                user=self.request.user,
+                data_date__year=year,
+                data_date__month=month,
+                active=True)
+            for ride in rides:
+                miles_this_year += ride.data["distance"]
+            milage.append(round(miles_this_year))
+
+        context["labels"] = month_year
+        context["data"] = milage
+        context["bgColor"] = bgColor
+        context["chart_title"] = "Distance (KM) by Month"
+        return context
 
 
 class db_year(LoginRequiredMixin, TemplateView):
