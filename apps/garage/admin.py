54cdee1c349993694ac30dbc803adb1844297a5c
Tim Andrews <timpandrews@yahoo.com>
2023-02-24 14:55:29 -0500

    add kudos model

--- apps/garage/admin.py
+++ apps/garage/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
-from .models import Doc
+from .models import Doc, Kudos
 
 
 class DocAdmin(admin.ModelAdmin):
     fieldsets = [
         ("Fieldset", {
             "fields": [
+                "id",
                 "user",
                 "data_type",
                 "data_date",
@@ -30,5 +31,37 @@ class DocAdmin(admin.ModelAdmin):
         "active",
     )
     list_display_links = ("id", "user", "data_type")
-
 admin.site.register(Doc, DocAdmin)
+
+
+class KudosAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Fieldset", {
+            "fields": [
+                "user",
+                "key",
+                "type",
+                "data",
+                "created",
+                "updated",
+                "active",
+                "placed",
+                ]
+            }
+        ),
+    ]
+    readonly_fields = ("created", "updated")
+    list_display = (
+        "id",
+        "key",
+        "user",
+        "type",
+        "data",
+        "created",
+        "updated",
+        "active",
+        "placed",
+    )
+    list_display_links = ("id", "key")
+
+admin.site.register(Kudos, KudosAdmin)
--- apps/garage/models.py
+++ apps/garage/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+KUDOS_TYPES = (
+    ("App", "app"), # Application Kudos
+    ("Rides", "rides"),
+    ("HP", "hp"), # health points
+)
+
 
 class Doc(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -16,3 +22,20 @@ class Doc(models.Model):
 
     def __str__(self):
         return self.data_type
+
+
+class Kudos(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    key = models.CharField(max_length=20)
+    type = models.CharField(max_length=20, choices=KUDOS_TYPES,)
+    data = models.JSONField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    placed = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Kudos"
+
+    def __str__(self):
+        return self.key
