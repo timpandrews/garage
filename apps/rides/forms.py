e2027bb0b52134da2dffd7123e653a0c3939298b
Tim Andrews <timpandrews@yahoo.com>
2023-02-24 13:13:49 -0500

    SplitDateTime Picker & Remove initial values

--- apps/rides/forms.py
+++ apps/rides/forms.py
@@ -1,38 +1,35 @@
 from django import forms
 from django.forms import ModelForm
+from django.contrib.admin.widgets import AdminSplitDateTime
 
 from apps.garage.models import Doc
 
 
 class AddNewRide(ModelForm):
-    start = forms.DateTimeField(
-        label="Start Time/Day", help_text="%Y-%m-%d %H:%M:%S",
-        initial="2023-02-09 11:02:00",) # "%Y-%m-%d %H:%M:%S"
+    start = forms.SplitDateTimeField (
+        label="Start Date/Time", widget=AdminSplitDateTime()) # "%Y-%m-%d %H:%M:%S"
     ride_title = forms.CharField(
-        label="Ride Title", max_length=200, initial="Ride Title Test",
-        required=False,)
+        label="Ride Title", max_length=200, required=False,)
     route = forms.CharField(
-        label="Route", max_length=200, initial="Route Info",required=False)
+        label="Route", max_length=200, required=False)
     equipment = forms.CharField(
-        label="Equipment", max_length=200,
-        initial="Info about your bike, trainer, etc.", required=False,)
+        label="Equipment", max_length=200, required=False,)
     notes = forms.CharField(
-        label="Notes", max_length=200, initial="Notes", required=False)
-    distance = forms.FloatField(label="Distance", min_value=0, initial=12.3)
-    duration = forms.DurationField(
-        label="Ride Duration", initial="1:2:3", help_text="h:m:s")
-    elevation = forms.IntegerField(label="Elevation Gained", min_value=0, initial=123)
-    weighted_power_avg = forms.IntegerField(label="Weighted Power Average", min_value=0, initial=123)
-    total_work = forms.IntegerField(label="Total Work", min_value=0, initial=123)
-    speed_avg = forms.FloatField(label="Average Speed", min_value=0, initial=12.3)
-    speed_max = forms.FloatField(label="Max Speed", min_value=0, initial=12.3)
-    hr_avg = forms.IntegerField(label="Average Heart Rate", min_value=0, initial=123)
-    hr_max = forms.IntegerField(label="Max Heart Rate", min_value=0, initial=123)
-    cadence_avg = forms.IntegerField(label="Average Cadence", min_value=0, initial=123)
-    cadence_max = forms.IntegerField(label="Max Cadence", min_value=0, initial=123)
-    power_avg = forms.IntegerField(label="Average Power", min_value=0, initial=123)
-    power_max = forms.IntegerField(label="Max Power", min_value=0, initial=123)
-    calories = forms.IntegerField(label="Calories", min_value=0, initial=123)
+        label="Notes", max_length=200, required=False)
+    distance = forms.FloatField(label="Distance", min_value=0,)
+    duration = forms.DurationField(label="Ride Duration", help_text="h:m:s")
+    elevation = forms.IntegerField(label="Elevation Gained", min_value=0,)
+    weighted_power_avg = forms.IntegerField(label="Weighted Power Average", min_value=0,)
+    total_work = forms.IntegerField(label="Total Work", min_value=0,)
+    speed_avg = forms.FloatField(label="Average Speed", min_value=0,)
+    speed_max = forms.FloatField(label="Max Speed", min_value=0,)
+    hr_avg = forms.IntegerField(label="Average Heart Rate", min_value=0,)
+    hr_max = forms.IntegerField(label="Max Heart Rate", min_value=0,)
+    cadence_avg = forms.IntegerField(label="Average Cadence", min_value=0,)
+    cadence_max = forms.IntegerField(label="Max Cadence", min_value=0,)
+    power_avg = forms.IntegerField(label="Average Power", min_value=0,)
+    power_max = forms.IntegerField(label="Max Power", min_value=0,)
+    calories = forms.IntegerField(label="Calories", min_value=0,)
 
     class Meta:
         model = Doc
--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from django.views.i18n import JavaScriptCatalog
+
 urlpatterns = [
+    path("jsil8n", JavaScriptCatalog.as_view(), name="js-catalog"),
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path('', include('apps.garage.urls')),
