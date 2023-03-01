c12f0102caa36390910c0f8e232d43dd3cdfe69d
Tim Andrews <timpandrews@yahoo.com>
2023-03-01 15:21:11 -0500

    change model name to UserProfile, added fields

--- apps/garage/models.py
+++ apps/garage/models.py
@@ -43,10 +43,12 @@ class Kudos(models.Model):
         return self.key
 
 
-class Profile(models.Model):
+class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email_confirmed = models.BooleanField(default=False)
-    test = models.CharField(max_length=25)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
-        return self.user.username
+        return f'{self.user.username} Profile'
