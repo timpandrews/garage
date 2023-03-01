265a150cbc72548146602d66a83b6a211dc06f04
Tim Andrews <timpandrews@yahoo.com>
2023-03-01 09:52:23 -0500

    Add Profile model & signal to create a profile for each new user

--- apps/garage/signals.py
+++ apps/garage/signals.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.models import User
 from django.db.models import signals
 from django.dispatch import receiver
-from .models import Kudos
+
+from .models import Kudos, Profile
+
 
 @receiver(signals.post_save, sender=Kudos)
 def create_hexkey(sender, instance, created, **kwargs):
@@ -8,3 +11,10 @@ def create_hexkey(sender, instance, created, **kwargs):
     hexuser = hex(instance.user_id)[2:].zfill(3)
     key = f"{hexuser}_{hexid}"
     Kudos.objects.filter(pk=instance.id).update(hex=hexid, key=key)
+
+
+@receiver(signals.post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        instance.profile.save()
