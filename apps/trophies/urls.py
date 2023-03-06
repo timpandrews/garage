9d77f20afe66072f67f1b7654f5241897b160c91
Tim Andrews <timpandrews@yahoo.com>
2023-03-06 00:08:01 -0500

    Create url for trophies. redirect to trophies_edit

--- apps/trophies/urls.py
+++ apps/trophies/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import trophies_edit, trophies_view
+from .views import trophies_edit, trophies_view, TrophiesRedirectView
 
 # app_name = 'trophies'
 
 urlpatterns = [
+    path("trophies/", TrophiesRedirectView.as_view(), name="trophies"),
     path("trophies/edit/<int:user_id>", trophies_edit, name="trophies_edit"),
     path("trophies/view/<int:user_id>", trophies_view, name="trophies_view"),
 ]
