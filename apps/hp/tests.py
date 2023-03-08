974bbe1cbe6f4b36c6342a78230df659308ea4f7
Tim Andrews <timpandrews@yahoo.com>
2023-03-08 16:10:30 -0500

    Tests???

--- apps/hp/tests.py
+++ apps/hp/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Test for the HPListView
+class HPListViewTest(TestCase):
+    def test_hp_list_view(self):
+        response = self.client.get('/hp/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'hp/hp_list.html')
+
