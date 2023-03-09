2bd33127b8927ff78c8e4655ba1543da3f098828
Tim Andrews <timpandrews@yahoo.com>
2023-03-09 14:22:49 -0500

    enable namespace for trophies rename urls to reflect the use of namespace

--- apps/trophies/urls.py
+++ apps/trophies/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from .views import trophies_edit, trophies_view, TrophiesRedirectView
 
-# app_name = 'trophies'
+app_name = 'trophies'
 
 urlpatterns = [
     path("trophies/", TrophiesRedirectView.as_view(), name="trophies"),
-    path("trophies/edit/<int:user_id>", trophies_edit, name="trophies_edit"),
-    path("trophies/view/<int:user_id>", trophies_view, name="trophies_view"),
+    path("trophies/edit/<int:user_id>", trophies_edit, name="edit"),
+    path("trophies/view/<int:user_id>", trophies_view, name="view"),
 ]
