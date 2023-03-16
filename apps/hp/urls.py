1f635892182c540afb74e585582ffe4a658b2279
Tim Andrews <timpandrews@yahoo.com>
2023-03-15 22:50:14 -0400

    update for consistent use of urls

--- apps/hp/urls.py
+++ apps/hp/urls.py
@@ -6,7 +6,7 @@ from .views import *
 app_name = 'hp'
 
 urlpatterns = [
-    path('hp/', views.HPListView.as_view(), name='all'),
+    path('hp/', views.HPListView.as_view(), name='list'),
     path('hp/<int:pk>/detail/', views.HPDetailView.as_view(), name='hp_detail'),
     path('hp/create/', views.HPCreateView.as_view(), name='hp_create'),
     path('hp/create/<str:hp_type>/', views.HPCreateView.as_view(), name='hp_create'),
