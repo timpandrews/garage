6539fe042ed20e283b463b30026dc1f56ac3a24d
Tim Andrews <timpandrews@yahoo.com>
2023-03-17 15:51:59 -0400

    Change url names in HP app to match pattern used in other apps

--- apps/hp/urls.py
+++ apps/hp/urls.py
@@ -7,10 +7,10 @@ app_name = 'hp'
 
 urlpatterns = [
     path('hp/', views.HPListView.as_view(), name='list'),
-    path('hp/<int:pk>/detail/', views.HPDetailView.as_view(), name='hp_detail'),
-    path('hp/create/', views.HPCreateView.as_view(), name='hp_create'),
-    path('hp/create/<str:hp_type>/', views.HPCreateView.as_view(), name='hp_create'),
-    path('hp/<int:pk>/update/', views.HPUpdateView.as_view(), name='hp_update'),
-    path('hp/<int:pk>/update/<str:hp_type>/', views.HPUpdateView.as_view(), name='hp_update'),
-    path('hp/<int:pk>/delete/', views.HPDeleteView.as_view(), name='hp_delete'),
+    path('hp/<int:pk>/detail/', views.HPDetailView.as_view(), name='detail'),
+    path('hp/create/', views.HPCreateView.as_view(), name='create'),
+    path('hp/create/<str:hp_type>/', views.HPCreateView.as_view(), name='create'),
+    path('hp/<int:pk>/update/', views.HPUpdateView.as_view(), name='update'),
+    path('hp/<int:pk>/update/<str:hp_type>/', views.HPUpdateView.as_view(), name='update'),
+    path('hp/<int:pk>/delete/', views.HPDeleteView.as_view(), name='delete'),
 ]
