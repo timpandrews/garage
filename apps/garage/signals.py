11eb9d4a3ca2d93a2e29be747c85da4b539269fd
Tim Andrews <timpandrews@yahoo.com>
2023-05-30 09:18:08 -0400

    Create signal that will created a 1st activity of doc_type="joined"

--- apps/garage/signals.py
+++ apps/garage/signals.py
@@ -1,8 +1,9 @@
+from datetime import datetime, timezone
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import Kudos, Profile
+from .models import Doc, Kudos, Profile
 
 
 @receiver(post_save, sender=Kudos)
@@ -15,6 +16,26 @@ def create_hexkey(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
+    # Create a profile for a user when the user is created
     if created:
         Profile.objects.create(user=instance)
         instance.profile.save()
+
+
+@receiver(post_save, sender=User)
+def create_first_activity(sender, instance, created, **kwargs):
+    # create a first activity for a user when the user is created
+    print("create_first_activity")
+    if created:
+        dt = datetime.now()
+        dt = dt.replace(tzinfo=timezone.utc)
+        Doc.objects.create(
+            user=instance,
+            doc_type="joined",
+            doc_date=dt,
+        )
+
+
+# TODO: create signal to create a trophy for joining the site, cause everyone gets a trophy!
+# def create_welcome_to_the_site_trophy...
+
