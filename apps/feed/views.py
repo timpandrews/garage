16d7699c6507714dfd53cd70e972374c91046f14
Tim Andrews <timpandrews@yahoo.com>
2023-06-29 10:37:40 -0400

    add display_pref & uni_names to feed & detail views

--- apps/feed/views.py
+++ apps/feed/views.py
@@ -71,6 +71,7 @@ class DetailView(LoginRequiredMixin, DetailView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(DetailView, self).get_context_data(*args, **kwargs)
+        user=self.request.user
 
         chart = {}
 
@@ -78,13 +79,33 @@ class DetailView(LoginRequiredMixin, DetailView):
         doc = Doc.objects.get(id=doc_id)
 
         activity = clean_data_for_display(doc.data)
+
         activity_type = doc.doc_type
+
+        if user.profile.units_display_preference == "imperial":
+                activity = convert_to_imperial(activity, activity_type)
+
         if activity_type == "ride":
             activity["map"] = build_map(doc)
-            chart["labels"], chart["elevation"] = build_elevation_chart(doc)
+            chart["labels"], chart["elevation"] = build_elevation_chart(
+                doc, user.profile.units_display_preference
+            )
+
+        chart["test"] = "test"
+        if user.profile.units_display_preference == "imperial":
+            chart["y_label"] = "Distance (miles)"
+            chart["x_label"] = "Elevation (ft)"
+        else: # metric
+            chart["y_label"] = "Distance (km)"
+            chart["x_label"] = "Elevation (m)"
 
         context["chart"] = chart
+
+
         context["doc_type"] = doc.doc_type
         context["activity"] = activity
+
+        context["display_pref"] = user.profile.units_display_preference
+        context["unit_names"] = get_unit_names(user.profile.units_display_preference)
 
         return context
