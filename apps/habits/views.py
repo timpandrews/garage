d9ee4dea9540087fe115cc3c5e6ae2ad6cd5a44c
Tim Andrews <timpandrews@yahoo.com>
2023-10-26 14:57:29 -0400

    Stub for manage habits page

--- apps/habits/views.py
+++ apps/habits/views.py
@@ -1,4 +1,13 @@
 from django.views.generic import TemplateView
+from apps.garage.models import Profile
+
 
 class Habits(TemplateView):
     template_name = "habits/manage.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        habits = Profile.objects.filter(user=self.request.user).values("habits").first()
+        context["habits"] = habits["habits"]
+        # print(habits)
+        return context
